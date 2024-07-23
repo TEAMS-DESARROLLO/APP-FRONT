31fa04445b81d5c65c56c6b54bc5d45edb6de727
Carlos Rojas <carojasc@indracompany.com>
2024-07-23 11:08:17 -0500

    update developer

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   nombreApp : "Entorno Desarrollo",
   // host:"www.micloud2.com",
-  host:"http://172.210.178.174/",
+  host:"http://172.210.178.174",
 
   port:":7001",
   context:"",
