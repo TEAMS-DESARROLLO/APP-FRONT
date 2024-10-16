ca194e251a6ec556f6c823eaccd9ccf8437279ce
gfachuy <gfachuy@indracompany.com>
2024-10-16 10:48:01 -0500

    fix environment development

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   nombreApp : "Entorno Desarrollo",
   protocol:"http://",
   //host:"www.micloud2.com",
-  host:"localhost",//"172.210.178.174",
+  host:"172.210.178.174",
   port:":7001",
   context:"",
   hostGateWay:"172.210.178.174",
